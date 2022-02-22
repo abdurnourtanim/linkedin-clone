8192c6eca6dedcf9007ffbc190237a1638b89d03
antanim <abdurnouutanim11@gmail.com>
2022-02-22 13:46:01 +0600

    added middleware

---
+++ pages/_middleware.js
@@ -0,0 +1,22 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+  if (req.nextUrl.pathname === "/") {
+    const session = await getToken({
+      req,
+      secret: process.env.JWT_SECRET,
+      secureCookie: process.env.NODE_ENV === "production",
+    });
+
+    if (!session) {
+      const url = req.nextUrl.clone();
+      url.pathname = "/home";
+      return NextResponse.redirect(url);
+    }
+  }
+
+  //   const url = req.nextUrl.clone();
+  //   url.pathname = "/";
+  //   return NextResponse.rewrite(url);
+}
