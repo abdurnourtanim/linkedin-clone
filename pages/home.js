3e79ada20faa7132c25e92d4e95c95f1ff6ede3c
antanim <abdurnouutanim11@gmail.com>
2022-02-22 11:53:51 +0600

    home page main content done

--- pages/home.js
+++ pages/home.js
@@ -1,3 +1,4 @@
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenterSharp";
 import ExploreIcon from "@mui/icons-material/Explore";
 import GroupIcon from "@mui/icons-material/Group";
@@ -8,16 +9,10 @@ import HeaderLink from "../components/HeaderLink";
 
 const Home = () => {
   return (
-    <div>
+    <div className="space-y-10 relative">
       <header className="flex justify-around items-center py-4">
         <div className="relative w-36 h-10">
-          <Image
-            // src="https://rb.gy/vtbzlp"
-            src="/logo.png"
-            layout="fill"
-            objectFit="contain"
-            alt="logo"
-          />
+          <Image src="/logo.png" layout="fill" objectFit="contain" alt="logo" />
         </div>
         <div className="flex items-center sm:divide-x divide-gray-300">
           <div className="hidden sm:flex space-x-8 pr-4">
@@ -33,6 +28,36 @@ const Home = () => {
           </div>
         </div>
       </header>
+
+      <main className="flex flex-col xl:flex-row items-center max-w-screen-lg mx-auto">
+        <div className="space-y-6 xl:space-y-10">
+          <h1 className="text-3xl md:text-5xl text-amber-800/80 max-w-xl !leading-snug pl-4 xl:pl-0">
+            Welcome to your professional community
+          </h1>
+          <div className="space-y-4">
+            <div className="intent">
+              <h2 className="text-xl">Search for a job</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+            <div className="intent">
+              <h2 className="text-xl">Find a person you know</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+            <div className="intent">
+              <h2 className="text-xl">Learn a new skill</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+          </div>
+        </div>
+        <div className="relative xl:absolute w-80 h-80 xl:w-[650px] xl:h-[650px] top-14 right-5">
+          <Image
+            src="/linkedin-home.svg"
+            layout="fill"
+            priority
+            alt="linkedin-home"
+          />
+        </div>
+      </main>
     </div>
   );
 };
