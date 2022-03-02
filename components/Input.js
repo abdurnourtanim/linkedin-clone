657bf174f502c9063426b81bd5bee2413df84d60
antanim <abdurnouutanim11@gmail.com>
2022-03-03 00:10:51 +0600

    Added modal

--- components/Input.js
+++ components/Input.js
@@ -6,9 +6,14 @@ import { Avatar } from "@mui/material";
 import { motion } from "framer-motion";
 import { useSession } from "next-auth/react";
 import React from "react";
+import { useRecoilState } from "recoil";
+import { modalState, modalTypeState } from "../atoms/modalAtom";
 
 const Input = () => {
   const { data: session } = useSession();
+  const [modalOpen, setModalOpen] = useRecoilState(modalState);
+  const [modalType, setModalType] = useRecoilState(modalTypeState);
+
   return (
     <div className="bg-white dark:bg-[#1D2226] rounded-lg p-3 space-y-3 border border-gray-300 dark:border-none">
       <div className="flex items-center space-x-2">
@@ -21,8 +26,8 @@ const Input = () => {
           whileTap={{ scale: 0.99 }}
           className="rounded-full border border-gray-400 py-2.5 px-3 opacity-80 hover:opacity-100 font-medium w-full text-left"
           onClick={() => {
-            // setModalOpen(true);
-            // setModalType("dropIn");
+            setModalOpen(true);
+            setModalType("dropIn");
           }}
         >
           Start a post
