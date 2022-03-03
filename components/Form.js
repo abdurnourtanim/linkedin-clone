1a9d235c5304eac13e06304d866ab6e8da603540
antanim <abdurnouutanim11@gmail.com>
2022-03-04 00:24:02 +0600

    post route and post method backend kworking

--- components/Form.js
+++ components/Form.js
@@ -1,8 +1,42 @@
+import { useSession } from "next-auth/react";
 import React, { useState } from "react";
+import { useRecoilState } from "recoil";
+import { modalState } from "../atoms/modalAtom";
+import { handlePostState } from "../atoms/postAtom";
 
 const Form = () => {
   const [input, setInput] = useState("");
   const [photoUrl, setPhotoUrl] = useState("");
+  const { data: session } = useSession();
+  const { userName, email, image: userImg } = session.user;
+  const [modalOpen, setModalOpen] = useRecoilState(modalState);
+  const [handlePost, setHandlePost] = useRecoilState(handlePostState);
+
+  const uploadPost = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("/api/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        input,
+        photoUrl,
+        userName,
+        email,
+        userImg,
+        createdAt: new Date().toString(),
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const responseData = await response.json();
+
+    console.log(responseData);
+
+    setHandlePost(true);
+    setModalOpen(false);
+  };
 
   return (
     <form className="flex flex-col relative space-y-2 text-black/80 dark:text-white/75">
@@ -25,8 +59,8 @@ const Form = () => {
       <button
         className="absolute bottom-0 right-0 font-medium bg-blue-400 hover:bg-blue-500 disabled:text-black/40 disabled:bg-white/75 disabled:cursor-not-allowed text-white rounded-full px-3.5 py-1"
         type="submit"
-        // onClick={uploadPost}
-        // disabled={!input.trim() && !photoUrl.trim()}
+        onClick={uploadPost}
+        disabled={!input.trim() && !photoUrl.trim()}
       >
         Post
       </button>
