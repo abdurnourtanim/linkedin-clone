950583324a6d1ca050f57b25b96d5f5882c5fab2
antanim <abdurnouutanim11@gmail.com>
2022-03-07 00:08:28 +0600

    post component ready

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -1,6 +1,8 @@
+import { useSession } from "next-auth/react";
 import React from "react";
 
 const HeaderLink = ({ Icon, text, feed, active, avatar, hidden }) => {
+  const { data: session } = useSession();
   return (
     <div
       className={` ${
@@ -11,7 +13,11 @@ const HeaderLink = ({ Icon, text, feed, active, avatar, hidden }) => {
           : "text-gray-500 hover:text-gray-700"
       }  ${active && "!text-black dark:!text-white"} `}
     >
-      {avatar ? <Icon className="!h-7 !w-7 lg:!-mb-1" /> : <Icon />}
+      {avatar ? (
+        <Icon className="!h-7 !w-7 lg:!-mb-1" src={session?.user?.image} />
+      ) : (
+        <Icon />
+      )}
       <h4
         className={`text-sm ${
           feed && "hidden lg:flex justify-center w-full mx-auto"
